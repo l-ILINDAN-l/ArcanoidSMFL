29bb22e846628e34206d21b62a474bc3d6801dee
Ilya Gellert <gellertilya@gmail.com>
2025-05-25 21:04:02 +0300

    push github

--- src/Ball/Ball.h
+++ src/Ball/Ball.h
@@ -5,10 +5,22 @@
 #ifndef BALL_H
 #define BALL_H
 
-
+#include <box2d/box2d.h>
+#include <SFML/Graphics.hpp>
 
 class Ball {
-
+public:
+    Ball();
+    void launch();
+    void stickToPaddle();
+    void update(float dt);
+    void draw(sf::RenderWindow& target);
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    b2Body* m_body;
+    bool m_isSticky;
+    bool m_isOnPaddle;
 };
 
 
